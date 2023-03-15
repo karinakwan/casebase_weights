f9afe3e664a9eadc6055b21d0f110c9fd97e2d8d
karinakwan <84237799+karinakwan@users.noreply.github.com>
2023-03-15 11:23:20 -0400

    Restore weights to glm

--- R/fitting.R
+++ R/fitting.R
@@ -168,7 +168,7 @@ fitSmoothHazard <- function(formula, data, time,
   if (length(typeEvents) == 2) {
     fittingFunction <- switch(family,
       "glm" = function(formula) glm(formula, data = sampleData,
-                                    family = binomial),
+                                    family = binomial, weights = wts),
       "glmnet" = function(formula) cv.glmnet.formula(formula, sampleData,
                                                      event = eventVar, ...),
       "gam" = function(formula) mgcv::gam(formula, sampleData,
